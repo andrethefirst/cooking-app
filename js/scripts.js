fd3726e3f3f286483af964791b3e98116ad57bb0
Andre Velasquez <andrethefirst21@gmail.com>
2021-06-08 19:52:12 -0700

    added html base files

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+function gallonToQuart(number1) {
+  return number1 * 4;
+}
+
+function quartToPint(number1) {
+  return number1 * 2;
+}
+
+function pintToCup(number1) {
+  return number1 * 2;
+}
+
+function quartToGallon(number1) {
+  return number1 /4;
+}
+
+function pintToQuart(number1) {
+  return number1 / 2;
+}
+
+function cupToPint(number1) {
+  return number1 / 2;
+}
+function gallonToPint(number1) {
+  return number1 * 8;
+}
+function gallonToCups(number1) {
+  return number1 * 16;
+}
+
+function pintToGallon(number1) {
+  return number1 / 8;
+}
+
+function 
